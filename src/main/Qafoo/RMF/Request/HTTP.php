605a1f29f8dec25673dfe5461973585c42de09ae
Tobias Schlitt <toby@php.net>
2012-04-09 17:17:29 +0200

    Fixed: Retrieval of HTTP method.

--- src/main/Qafoo/RMF/Request/HTTP.php
+++ src/main/Qafoo/RMF/Request/HTTP.php
@@ -35,7 +35,7 @@ class HTTP extends Request
         $this->contents['path']        = $_SERVER['REQUEST_URI'];
         $this->contents['variables']   = $_GET;
 
-        $this->addHandler( 'method', new Request\PropertyHandler\Server( 'HTTP_METHOD' ) );
+        $this->addHandler( 'method', new Request\PropertyHandler\Server( 'REQUEST_METHOD' ) );
         $this->addHandler( 'language', new Request\PropertyHandler\AcceptHeader( 'HTTP_ACCEPT_LANGUAGE', 'en' ) );
         $this->addHandler( 'encoding', new Request\PropertyHandler\AcceptHeader( 'HTTP_ACCEPT_CHARSET', 'utf-8' ) );
         $this->addHandler( 'compression', new Request\PropertyHandler\AcceptHeader( 'HTTP_ACCEPT_ENCODING', 'identity' ) );
